54f9868b8f68a69514906cc134f4d644f0e28dd1
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-08-17 15:11:48 +0300

    finished drawer layout

--- app/src/main/java/filo/mamdouh/kershhelper/HomeActivity.java
+++ app/src/main/java/filo/mamdouh/kershhelper/HomeActivity.java
@@ -1,33 +1,59 @@
 package filo.mamdouh.kershhelper;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     private static int savedNumber;
     private ImageButton drawer;
-    private Button savedButton;
+    private Button bookMarkBtn;
     private DrawerLayout mDrawerLayout;
+    AppCompatActivity homeActivity;
+    private OnBackPressedCallback callback;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        homeActivity = this;
         setContentView(R.layout.activity_home);
         mDrawerLayout =findViewById(R.id.HomeActivityLayout);
         drawer = findViewById(R.id.profileButton);
-        savedButton = findViewById(R.id.toolbarSavedBtn);
+        bookMarkBtn = findViewById(R.id.toolbarSavedBtn);
         drawer.setOnClickListener(l->
             mDrawerLayout.openDrawer(GravityCompat.START)
         );
+        mDrawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerClosed(View drawerView){
+                super.onDrawerClosed(drawerView);
+                //TODO
+            }
+        });
+        callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if(mDrawerLayout.isDrawerOpen(GravityCompat.START)) mDrawerLayout.closeDrawer(GravityCompat.START);
+                else {
+                    Toast.makeText(HomeActivity.this, "press Back Again to leave", Toast.LENGTH_SHORT).show();
+                    callback.setEnabled(false);
+                    new Handler(Looper.getMainLooper()).postDelayed(()->callback.setEnabled(true),5000);
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this,callback);
     }
 }
---
+++ app/src/main/java/filo/mamdouh/kershhelper/views/communicators/DrawerCommunicator.java
@@ -0,0 +1,5 @@
+package filo.mamdouh.kershhelper.views.communicators;
+
+public interface DrawerCommunicator {
+    public void toHomepage();
+}
